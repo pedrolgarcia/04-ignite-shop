a77b90025ca767ebd52a516add19dc2ff6925b10
Pedro Lopes <pedrolopesgrc@gmail.com>
2022-09-26 23:28:51 -0300

    feat: install stitches, global styles

---
+++ src/pages/index.tsx
@@ -0,0 +1,24 @@
+import { styled } from "../styles"
+
+const Button = styled("button", {
+  backgroundColor: "$green500",
+  borderRadius: 4,
+  border: 0,
+  padding: "4px 8px",
+
+  span: {
+    fontWeight: "bold"
+  },
+
+  transition: "filter 0.2s",
+
+  "&:hover": {
+    filter: "brightness(0.8)"
+  }
+})
+
+export default function Home() {
+  return (
+    <Button>Enviar</Button>
+  )
+}
