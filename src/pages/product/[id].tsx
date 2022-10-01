f5093dd8f8f9e488a78abb586262403b15e05d8d
Pedro Lopes <pedrolopesgrc@gmail.com>
2022-10-01 00:03:49 -0300

    feat: product page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,22 @@
 import { useRouter } from "next/router"
 
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
+
 export default function Product() {
   const { query } = useRouter()
 
   return (
-    <h1>{ JSON.stringify(query) }</h1>
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati molestiae incidunt eius, ut ratione autem. Sapiente quam iusto consectetur adipisci harum debitis suscipit iure eos maiores ea, commodi veritatis quos!</p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
