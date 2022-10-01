77d9d89b984e7e4f5a0b00032341726ef457cb60
Pedro Lopes <pedrolopesgrc@gmail.com>
2022-09-30 23:46:24 -0300

    feat: Link SPA behaviour

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import { HomeContainer, Product } from "../styles/pages/home";
 import { stripe } from "../lib/stripe";
 
 import "keen-slider/keen-slider.min.css"
+import Link from "next/link";
 
 interface HomeProps {
   products: {
@@ -29,14 +30,16 @@ export default function Home({ products }: HomeProps) {
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map((product) => (
-        <Product key={product.id} className="keen-slider__slide">
-          <Image src={product.imageUrl} width={520} height={480} alt="" />
+        <Link key={product.id} href={`/product/${product.id}`}>
+          <Product className="keen-slider__slide">
+            <Image src={product.imageUrl} width={520} height={480} alt="" />
 
-          <footer>
-            <strong>{product.name}</strong>
-            <span>{product.price}</span>
-          </footer>
-        </Product>
+            <footer>
+              <strong>{product.name}</strong>
+              <span>{product.price}</span>
+            </footer>
+          </Product>
+        </Link>
       ))}
     </HomeContainer>
   )
