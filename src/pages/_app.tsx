34a8f7291e640de28d87772b5d6f7e5e1cbbaa87
Pedro Lopes <pedrolopesgrc@gmail.com>
2022-10-06 21:12:27 -0300

    feat: cart button, creating cart component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,11 @@
 import { AppProps } from "next/app"
 import Image from "next/future/image"
+import { Handbag } from "phosphor-react"
 
 import { globalStyles } from "../styles/global"
-import { Container, Header } from "../styles/pages/app"
+import { CartBadge, CartButton, Container, Header } from "../styles/pages/app"
+
+import { Cart } from "../components/Cart"
 
 import logoImg from "../assets/logo.svg"
 
@@ -13,9 +16,19 @@ export default function App({ Component, pageProps }: AppProps) {
     <Container>
       <Header>
         <Image src={logoImg} alt="" />
+
+        <CartButton>
+          <Handbag size={24} weight="bold" />
+
+          <CartBadge>
+            <strong>1</strong>
+          </CartBadge>
+        </CartButton>
       </Header>
 
       <Component {...pageProps} />
+
+      <Cart />
     </Container>
   )
 }
--- src/styles/pages/app.ts
+++ src/styles/pages/app.ts
@@ -12,5 +12,57 @@ export const Header = styled("header", {
   padding: "2rem 0",
   width: "100%",
   maxWidth: 1180,
-  margin: "0 auto"
+  margin: "0 auto",
+  
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center"
+})
+
+export const CartButton = styled("button", {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+
+  position: "relative",
+  padding: "0.75rem",
+  borderRadius: 6,
+  border: 0,
+  outline: 0,
+  backgroundColor: "$gray800",
+
+  cursor: "pointer",
+
+  transition: "filter 0.2s",
+
+  "&:hover": {
+    filter: "brightness(0.8)"
+  },
+
+  svg: {
+    color: "$gray300",
+  }
+})
+
+export const CartBadge = styled("div", {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+
+  width: "1.5rem",
+  height: "1.5rem",
+  borderRadius: 24,
+  border: "3px solid $gray800",
+  backgroundColor: "$green500",
+
+  boxSizing: "content-box",
+
+  position: "absolute",
+  top: "-10px",
+  right: "-10px",
+
+  strong: {
+    fontSize: "0,875rem",
+    color: "$white"
+  }
 })
